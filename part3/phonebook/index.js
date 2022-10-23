adcc9320b005ad2540c249bf377bf97273ac349a
jacobdev03 <dev.jacob03@gmail.com>
2022-10-23 21:56:47 +0200

    ex 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 const Person = require("./models/persons");
+const { response } = require("express");
 app.use(express.static("build"));
 app.use(cors());
 
@@ -38,43 +39,28 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-// app.get("/api/persons/:id", (request, response) => {
-//   let id = Number(request.params.id);
-//   const person = persons.find((person) => person.id === id);
-//   if (person) {
-//     response.json(person);
-//   } else {
-//     response.status(404).end();
-//   }
-// });
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
+});
 
-// app.delete("/api/persons/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   persons = persons.filter((person) => person.id !== id);
-//   response.status(204).end();
-// });
-
-// app.post("/api/persons", (request, response) => {
-//   const body = request.body;
-//   const found = persons.find((p) => p.name === body.name);
-//   if (!body.name || !body.number) {
-//     return response.status(400).json({
-//       error: "The name or number is missing",
-//     });
-//   } else if (found) {
-//     return response.status(400).json({
-//       error: "The name already exists in the phonebook",
-//     });
-//   } else {
-//     const person = {
-//       name: body.name,
-//       number: body.number,
-//       id: generateId(),
-//     };
-//     persons = persons.concat(person);
-//     return response.json(person);
-//   }
-// });
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((err) => next(err));
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
